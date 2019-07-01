4ee1834803e54f0a7cbd38fc4f2115e0c2bd15ac
Kyle <kyle.wheeless@woz-u.com>
2019-07-01 16:13:05 -0700

    fix deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.200.32.253',
+      host: '54.191.225.21',
       username: 'ubuntu',
       privateKey: 'bfmain.pem'
     })
--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add . --all
-git commit -m "travis zoom integration"
+git commit -m "fix deploy.js"
 git push origin master
